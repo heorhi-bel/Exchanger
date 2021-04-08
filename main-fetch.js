e06343fc612d564a6097d35b2fbdb5ddfc25eebb
heorhi-bel <georgix@mail.ru>
2021-04-08 18:40:44 +0200

    copied main.js in main-fetch.js and modified/changed on fetch requsts

--- main-fetch.js
+++ main-fetch.js
@@ -1,59 +1,31 @@
 "use strict"
 
+let dropdownMenuEl = document.querySelector('.dropdown-menu')
+let dropdownEl = document.querySelector('.dropdown-toggle')
+let actualLiEl = document.querySelector('.active');
+
 let urlAdress = " https://v6.exchangerate-api.com/v6/40d62e170c028be72b38b1b1/latest/USD";
 
-let currancies = fetch(urlAdress)
-    .then( response => {
-        return response.json();
-    })
-
-console.log(currancies)
-
-setNameOfDropdown(currancies)
-
-function setNameOfDropdown(currency){
-    let actualLiEl = document.querySelector('.active');
-    
-    let dropdownEl = document.querySelector('.dropdown-toggle')
-    dropdownEl.textContent = currency.base_code;
-    
-    let dropdownMenuEl = document.querySelector('.dropdown-menu')
-    dropdownMenuEl.addEventListener('click', function(event){
-        actualLiEl.classList.remove('active')
-        actualLiEl = event.target
-        actualLiEl.classList.add('active')
-        dropdownEl.textContent = event.target.textContent.trim()
-
-        urlAdress = urlAdress.slice(0, 68) + event.target.textContent.trim()
-        console.log(urlAdress)
-        fetch(urlAdress)
-
-    })
+function apiUpdate(url){
+    fetch(url)
+    .then(response => response.json())
+    .then(currency => {
+        console.log(currency)
+        dropdownEl.textContent = currency.base_code;  
+        
+  })
 }
-// request.onload = function(){
-//     var currencies = request.response;
-//     console.log(currencies)
-//     setNameOfDropdown(currencies)
-// }
 
+apiUpdate(urlAdress);
 
-// function setNameOfDropdown(currency){
-//     let actualLiEl = document.querySelector('.active');
+  dropdownMenuEl.addEventListener('click', function(event){
+    actualLiEl.classList.remove('active')
+    actualLiEl = event.target
+    actualLiEl.classList.add('active')
+    dropdownEl.textContent = event.target.textContent.trim()
+
+    urlAdress = urlAdress.slice(0, 68) + event.target.textContent.trim()
+    console.log(urlAdress)
+    apiUpdate(urlAdress)
     
-//     let dropdownEl = document.querySelector('.dropdown-toggle')
-//     dropdownEl.textContent = currency.base_code;
-    
-//     let dropdownMenuEl = document.querySelector('.dropdown-menu')
-//     dropdownMenuEl.addEventListener('click', function(event){
-//         actualLiEl.classList.remove('active')
-//         actualLiEl = event.target
-//         actualLiEl.classList.add('active')
-//         dropdownEl.textContent = event.target.textContent.trim()
-
-//         urlAdress = urlAdress.slice(0, 68) + event.target.textContent.trim()
-//         console.log(urlAdress)
-//         request.open('GET', urlAdress)
-//         request.send()
-
-//     })
-// }
+})
